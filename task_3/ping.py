aa6bcdef96664d9bfb137ba6560e50e668fa46ca
cheru <72647130+c-h4ck3r@users.noreply.github.com>
2022-08-29 17:04:46 +0900

    Update ping.py

--- task_3/ping.py
+++ task_3/ping.py
@@ -3,8 +3,9 @@ os.system('clear')
 
 print('#' * 100)
 ip_to_check = input('IP to check: ')
+n = int(input("Enter the interval: "))
+print('-' * 60)
+for i in range(n):
+    os.system('ping {} -c 1'.format(ip_to_check))
 
-print('-' * 60)
-os.system('ping {}'.format(ip_to_check))
-print('-' * 60)
 input("Press any Key to Exit...")
